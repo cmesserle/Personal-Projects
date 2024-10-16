8d06e7b6eba33b385335ef43aadcb48bb8a415a4
Cassie Messerle <117544158+cmesserle@users.noreply.github.com>
2024-10-16 15:18:24 +0100

    Update db.js

--- cfgdegree-final-project/pip-pictures/server/db.js
+++ cfgdegree-final-project/pip-pictures/server/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2')
 const db = mysql.createConnection({
     host: "localhost",
     port: "3306",
-    user: "root", // Enter your local server username here
-    password: "CFG2023!", // Enter your local server password here
-    database: "pipdb",
+    user: "username", // Enter your local server username here
+    password: "password", // Enter your local server password here
+    database: "database_name",
   });
 
 module.exports = db;
